ddec84fea645327bd9f296010f1c2ad8508781b5
sakshi <sakshi.mistry@techcronus.com>
2023-04-25 14:40:09 +0530

    some changes

---
+++ frontend/src/actions/actions/cartAction.js
@@ -0,0 +1,54 @@
+import {
+    ADD_TO_CART,
+    REMOVE_CART_ITEM,
+    SAVE_SHIPPING_INFO,
+  } from "../constants/cartConstants";
+  import axios from "axios";
+  import { BASE_URL } from "../constants/globals";
+  
+  // Add to Cart
+  export const addItemsToCart = (id, quantity,token) => async (dispatch, getState) => {
+    const config = {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const { data } = await axios.get(
+      `http://127.0.0.1:4000/api/product/${id}`,config);
+  
+    dispatch({
+      type: ADD_TO_CART,
+      payload: {
+        product: data.product._id,
+        name: data.product.name,
+        price: data.product.price,
+        image: data.product.images[0].url,
+        stock: data.product.Stock,
+        quantity,
+      },
+    });
+  
+    // localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+  };
+  
+  // REMOVE FROM CART
+  export const removeItemsFromCart = (id) => async (dispatch, getState) => {
+    dispatch({
+      type: REMOVE_CART_ITEM,
+      payload: id,
+    });
+  
+    // localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+  };
+  
+  // SAVE SHIPPING INFO
+  export const saveShippingInfo = (data) => async (dispatch) => {
+    dispatch({
+      type: SAVE_SHIPPING_INFO,
+      payload: data,
+    });
+  
+    localStorage.setItem("shippingInfo", JSON.stringify(data));
+  };
